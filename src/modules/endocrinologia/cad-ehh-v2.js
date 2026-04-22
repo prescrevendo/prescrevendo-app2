2193b47893a28392dae04c1e42b22e9392fab63a
Luiz Alfredo <contato@prescrevendo.com>
2026-04-22 12:46:24 -0300

    feat: Adicionar novo tema CAD/EHH v2 com seção de Referências

---
+++ src/modules/endocrinologia/cad-ehh-v2.js
@@ -0,0 +1,19 @@
+export const cadEhhV2Data = {
+  id: "cad-ehh-v2",
+  nome: "Cetoacidose Diabetica e Estado Hiperglicemico Hiperosmolar v2",
+  orientacoes: [
+    {
+      titulo: "Criterios Diagnosticos",
+      itens: ["Glicemia >250 mg/dL"]
+    }
+  ],
+  referencias: [
+    {
+      numero: 1,
+      titulo: "Diabetes Care 2026",
+      autores: "ADA",
+      ano: 2026,
+      publicacao: "Diabetes Care"
+    }
+  ]
+}
--- src/modules/endocrinologia/index.js
+++ src/modules/endocrinologia/index.js
@@ -2,10 +2,12 @@ import { cadEhhData } from './cad-ehh.js'
 import { criseTireotoxicaData } from './crise-tireotoxica.js'
 import { hipoglicemiaData } from './hipoglicemia.js'
 import { insuficienciaAdrenalData } from './insuficiencia-adrenal.js'
+import { cadEhhV2Data } from './cad-ehh-v2.js'
 
 export const ENDOCRINOLOGIA_TEMAS = [
   cadEhhData,
   criseTireotoxicaData,
   hipoglicemiaData,
-  insuficienciaAdrenalData
+  insuficienciaAdrenalData,
+  cadEhhV2Data
 ]
